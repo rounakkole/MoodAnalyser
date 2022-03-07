10850d08ee3562c8d4e4c240a556bdf80494ab8f
rounak <rounakkole@gmail.com>
2022-03-07 22:55:36 +0530

    Add project files.

---
+++ MoodAnalyser/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace MoodAnalyser
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("mood analyser");
+        }
+    }
+}
